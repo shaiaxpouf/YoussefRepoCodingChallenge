68571769203a13c92584029b5badc3252dba4da4
achraf.abdelrhafour <achraf.abdelrhafour@IC-CS233-1.uc.ucalgary.ca>
2022-07-23 03:13:12 -0600

    Added my branch label.

--- src/application/Main.java
+++ src/application/Main.java
@@ -12,6 +12,7 @@ public class Main extends Application {
 		try {
 			VBox root = new VBox();
 			root.getChildren().add(new Label ("Achraf's Label"));
+			root.getChildren().add(new Label ("Achraf's branch"));
 			Scene scene = new Scene(root,400,400);
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("Youssef's project");
